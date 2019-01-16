7bce22d0db5909c3c4ae0f339452b27ed7a7e670
98789 <lerht@hotmail.com>
2019-01-16 15:05:52 -0500

    Added test for get_offline, updated the existing tests with the new date format

--- tests/test.py
+++ tests/test.py
@@ -4,15 +4,19 @@ from context import btc2fiat
 class TestBTCMethods(unittest.TestCase):
 
     def test_coindesk(self):
-        price = btc2fiat.get_coindesk(0.3, '20181008')
+        price = btc2fiat.get_coindesk(0.3, '2018-10-08')
         self.assertAlmostEqual(price, 1986.72039)
 
     def test_coinmarketcap(self):
-        price = btc2fiat.get_coinmarketcap(0.3, '20181008')
+        price = btc2fiat.get_coinmarketcap(0.3, '2018-10-08')
         self.assertAlmostEqual(price, 1995.669)
 
+    def test_offline(self):
+        price = btc2fiat.get_offline(0.3, '2018-10-08')
+        self.assertAlmostEqual(price, 1981.425)
+
     def test_value(self):
-        price = btc2fiat.get_value(0.3, '20181008')
+        price = btc2fiat.get_value(0.3, '2018-10-08')
         self.assertAlmostEqual(price, 1986.72039)
 
 if __name__ == '__main__':
